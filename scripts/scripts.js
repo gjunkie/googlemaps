a1a4756ef34854f790afdd2ac64f42529c6f0038
Freddie Carthy <fcarthy@gmail.com>
2015-04-28 09:58:33 -0700

    Add info window to markers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 
 /* Set up map and markers */
-function initialize() {
+(function initialize() {
+  // instantiate the map with the initial center coordinates
   var map = new google.maps.Map(document.getElementById("googleMap"), {
     center: { 
       "lat": 46.075136001950504, 
@@ -9,7 +10,20 @@ function initialize() {
     zoom: 9
   });
 
+  // load json data
   map.data.loadGeoJson('https://api.myjson.com/bins/2c42x');
-};
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  var infowindow = new google.maps.InfoWindow({
+    content: ""
+  });
+
+  // click event for each marker
+  map.data.addListener('click', function(event) {
+      var title = event.feature.getProperty("title");
+      var desc = event.feature.getProperty("description");
+      infowindow.setContent("<div style='width:150px; margin-left:20px; text-align:center;'><h3>"+title+"</h3><p>"+desc+"</p></div>");
+      infowindow.setPosition(event.feature.getGeometry().get());
+      infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)});
+      infowindow.open(map);
+  });
+})();
